9188526c2227ad92361d76123f8383410dfd39a8
SawshaDev <contactcallimarie@gmail.com>
2022-11-07 23:18:40 -0800

    teehee

--- stuff/src/main.rs
+++ stuff/src/main.rs
@@ -1,16 +1,13 @@
-use std::collections::HashMap;
-use std::{vec::Vec};
+use std::vec::Vec;
 
 fn main() {
-    let mut my_hash: HashMap<i32, &str> = HashMap::new();
+    let mut list: Vec<i32> = Vec::new();
 
-    for i in 1..9 {
-        my_hash.insert(i, "sdas");
-    }
-    let mut count_vec: Vec<_> = my_hash.iter().collect();
-    count_vec.sort_by(|x,y| x.0.cmp(&y.0));
-    for (k, v)in count_vec {
-        println!("{}: {}", k, v)
+    for i in 1..11 {
+        list.push(i)
     }
 
-}
+    for elem in list.iter() {
+        println!("{}", elem)
+    }
+}   
